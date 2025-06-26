1a9ac97870e5377b68c3def99b8c25305411f7cc
FAI <FaiOnayq@outlook.sa>
2025-06-26 07:52:37 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,12 +6,7 @@ import { getFirestore, collection, getDocs, addDoc, updateDoc, deleteDoc, doc } 
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC_6IwA0cHjuU3n_dqCGTqkNpNQQ5eyeTw",
-  authDomain: "final-5766c.firebaseapp.com",
-  projectId: "final-5766c",
-  storageBucket: "final-5766c.firebasestorage.app",
-  messagingSenderId: "330416121797",
-  appId: "1:330416121797:web:1072f88fcd104917b04dac"
+//
 };
 
 // Initialize Firebase
